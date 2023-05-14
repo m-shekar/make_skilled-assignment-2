5d12751a24f098ecefee6f8517c5f2795fd9a23e
mergu <mshekar9014@gmail.com>
2023-05-14 16:45:49 +0530

    plaindrome using if else condition

--- sky.py
+++ sky.py
@@ -1,4 +1,16 @@
-a = 'tenet'
-b = 'neuron'
-print(a == a[::-1])
-print(b ==  b[::-1])
+a = input("Enter a string: ")
+b = input("Enter b string: ")
+c = input("Enter c string: ")
+#Checking whether above words are palindrome or not
+if(a == a[::-1]):
+    print(f"{a} is palindrome")
+else:
+    print(f"{a} is not palindrome")
+if(b == b[::-1]):
+    print(f"{b} is palindrome")
+else:
+    print(f"{b} is not palindrome")
+if(c == c[::-1]):
+    print(f"{c} is palindrome")
+else:
+    print(f"{c} is not palindrome")
